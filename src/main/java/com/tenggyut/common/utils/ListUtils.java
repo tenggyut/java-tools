ddc90fcfc55e100a623530fff7c7620735a7a05d
tenggyt <tengyutong0213@gmail.com>
2015-09-29 15:37:59 +0800

    add method : interaction

--- src/main/java/com/tenggyut/common/utils/ListUtils.java
+++ src/main/java/com/tenggyut/common/utils/ListUtils.java
@@ -1,5 +1,6 @@
 package com.tenggyut.common.utils;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.tenggyut.common.logging.LogFactory;
 import org.apache.logging.log4j.Logger;
@@ -17,5 +18,9 @@ public class ListUtils {
     public static <T> boolean isTheSame(List<T> listOne, List<T> listTwo) {
         Set<T> intersection = Sets.intersection(Sets.newHashSet(listOne), Sets.newHashSet(listTwo));
         return listOne.size() == intersection.size() && listTwo.size() == intersection.size();
+    }
+
+    public static <T> List<T> interaction(List<T> listOne, List<T> listTwo) {
+        return Lists.newArrayList(Sets.intersection(Sets.newHashSet(listOne), Sets.newHashSet(listTwo)));
     }
 }
