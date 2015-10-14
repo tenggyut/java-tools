f2f9f21de36b0dd8e88b1b832b604dc5be6f4bd9
tenggyt <tengyutong0213@gmail.com>
2015-10-14 11:14:24 +0800

    refactor: move readResource to ResourceFinder

--- src/main/java/com/tenggyut/common/utils/FileUtils.java
+++ src/main/java/com/tenggyut/common/utils/FileUtils.java
@@ -2,18 +2,12 @@ package com.tenggyut.common.utils;
 
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
-import com.google.common.io.CharStreams;
 import com.google.common.io.Files;
 import com.tenggyut.common.logging.LogFactory;
-import com.tenggyut.config.ResourceFinder;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
 
 /**
  * File utility
@@ -55,34 +49,5 @@ public class FileUtils {
     public static void write(byte[] content, File out) throws IOException {
         Preconditions.checkArgument(out.exists(), "file " + out.getName() + "does not exist");
         Files.write(content, out);
-    }
-
-    /**
-     * read resource file
-     *
-     * @param resource resource file path
-     * @return resource file content.
-     */
-    public static List<String> readResource(String resource) {
-        InputStream in;
-        try {
-            in = ResourceFinder.findResources(resource);
-        } catch (IOException e) {
-            LOG.error("failed to find {}", resource);
-            return Lists.newArrayListWithCapacity(0);
-        }
-        InputStreamReader inr = new InputStreamReader(in);
-        try {
-            return StringsUtils.split(CharStreams.toString(inr), DEFAULT_LINE_SEPARATOR);
-        } catch (IOException e) {
-            LOG.error("failed to read {} ", resource);
-            return Lists.newArrayListWithCapacity(0);
-        } finally {
-            try {
-                in.close();
-            } catch (IOException e) {
-                LOG.error("failed to close InputStream because {}", e.getMessage());
-            }
-        }
     }
 }
--- src/main/java/com/tenggyut/config/ResourceFinder.java
+++ src/main/java/com/tenggyut/config/ResourceFinder.java
@@ -1,22 +1,31 @@
 package com.tenggyut.config;
 
 import com.google.common.base.StandardSystemProperty;
+import com.google.common.collect.Lists;
+import com.google.common.io.CharStreams;
 import com.google.common.io.Resources;
 import com.tenggyut.common.logging.LogFactory;
+import com.tenggyut.common.utils.FileUtils;
+import com.tenggyut.common.utils.StringsUtils;
 import org.apache.logging.log4j.Logger;
-import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 
 public class ResourceFinder {
     private static final Logger LOG = LogFactory.getLogger(ResourceFinder.class);
 
+    /**
+     * find the resource by the given fileName. this method will search classpath root(/) first. if nothing found,
+     * it will search the entire classpath.
+     *
+     * @param fileName the required resource path
+     * @return resource
+     * @throws IOException
+     */
     public static InputStream findResources(String fileName) throws IOException {
         URL url;
         try {
@@ -34,6 +43,35 @@ public class ResourceFinder {
             return url.openStream();
         } else {
             throw new IOException("something is wrong when loading the file " + fileName);
+        }
+    }
+
+    /**
+     * read resource file
+     *
+     * @param resource resource file path
+     * @return resource file content.
+     */
+    public static List<String> readResource(String resource) {
+        InputStream in;
+        try {
+            in = findResources(resource);
+        } catch (IOException e) {
+            LOG.error("failed to find {}", resource);
+            return Lists.newArrayListWithCapacity(0);
+        }
+        InputStreamReader inr = new InputStreamReader(in);
+        try {
+            return StringsUtils.split(CharStreams.toString(inr), FileUtils.DEFAULT_LINE_SEPARATOR);
+        } catch (IOException e) {
+            LOG.error("failed to read {} ", resource);
+            return Lists.newArrayListWithCapacity(0);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                LOG.error("failed to close InputStream because {}", e.getMessage());
+            }
         }
     }
 }
