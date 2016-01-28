fbd4a15fa0495f45d86515039f32696cd4c41f04
tenggyt <tengyutong0213@gmail.com>
2016-01-28 19:51:20 +0800

    optimize retryworker

--- src/main/java/com/tenggyut/os/RetryWorker.java
+++ src/main/java/com/tenggyut/os/RetryWorker.java
@@ -11,22 +11,25 @@ import org.apache.logging.log4j.Logger;
 public abstract class RetryWorker<T> {
     private static final Logger LOG = LogFactory.getLogger(RetryWorker.class);
     private static final int DEFAULT_MAX_RETRY = 5;
+    private static final long DEFAULT_GAP = 200;
 
     private int maxRetry;
     private String workName;
     private Optional<T> result;
+    private long gap;
 
-    public RetryWorker(String workName, int maxRetry) {
+    public RetryWorker(String workName, int maxRetry, long gap) {
         this.maxRetry = maxRetry;
         this.workName = workName;
+        this.gap = gap;
     }
 
     public RetryWorker() {
-        this("unnamed work", DEFAULT_MAX_RETRY);
+        this("unnamed work", DEFAULT_MAX_RETRY, DEFAULT_GAP);
     }
 
     public RetryWorker(String workName) {
-        this(workName, DEFAULT_MAX_RETRY);
+        this(workName, DEFAULT_MAX_RETRY, DEFAULT_GAP);
     }
 
     public T doWorkWithRetry() throws RetryFailedException {
@@ -38,6 +41,12 @@ public abstract class RetryWorker<T> {
                 break;
             }
             retry++;
+
+            try {
+                Thread.sleep(gap);
+            } catch (InterruptedException e) {
+                LOG.error("failed to pause a retry worker {} due to {}", e, workName);
+            }
         } while (retry < maxRetry);
 
         if (result.isPresent()) {
